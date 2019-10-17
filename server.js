d47da2403fc4983a41e80e60ebbed5e7ed644ace
kylebot0 <kylebot07@gmail.com>
2019-10-17 09:40:25 +0200

    Add react

--- server.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv').config()
-
-const port = process.env.PORT
-
-app
-    .use('/', express.static(path.join(__dirname, 'public')))
-    .use(require('./routes/router'))
-    .listen(port, () => console.log(`Server listening on port: ${port}`))
-    ;
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {
+    Component
+} from 'react'
+import ReactDOM from 'react-dom';
+import './index.css';
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default App
