c4d5af4dc2ece3c67269bea1d5da9e7c66ca3b37
kylebot0 <kylebot07@gmail.com>
2019-10-31 09:56:36 +0100

    Fix timeline not accepting new dates

--- client/src/components/timeline.js
+++ client/src/components/timeline.js
@@ -30,15 +30,44 @@ export class timeline extends Component {
              );
          };
 
-        getDerivedStateFromProps(props, state) {
+        static getDerivedStateFromProps(props, state) {
            if (props.stage !== state.stage) {
                let date = props.render[props.stage].date.value;
                console.log(date);
-               console.log(this)
-               let dateArray = this.offsetDate(date);
-               
+                 let dateArray = [];
+
+               let offsetDate = () => {
+                 for (let i = 0; i < 9; i++) {
+                   //Offset van -50 tot max 50
+                   let min = Math.ceil(-50);
+                   let max = Math.floor(50);
+                   let offset =
+                     Math.floor(Math.random() * (max - min + 1)) + min;
+                   if (date > 1963) {
+                     offset = Math.floor(offset / 2);
+                     let dateMax = date + offset;
+                     console.log(dateMax);
+                     if (dateMax > 2019) {
+                       date = 2019;
+                       dateArray.push(
+                         date - Math.floor(Math.random() * 50 + 1)
+                       );
+                     } else {
+                       dateArray.push(date + offset);
+                     }
+                   } else {
+                     dateArray.push(date + offset);
+                   }
+                 }
+                 dateArray.push(date);
+                 dateArray.sort(function(a, b) {
+                   return a - b;
+                 });
+                 console.log(dateArray);
+               };
+               offsetDate();
              return {
-               stage: props.stage,
+               stage: state.stage,
                data: {
                  date: props.render[props.stage].date.value,
                  dateArray: dateArray
@@ -97,7 +126,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="2">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[1] > -50
                      ? this.state.data.dateArray[1]
                      : "leeg"}
@@ -105,7 +134,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="3">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[2] > -50
                      ? this.state.data.dateArray[2]
                      : "leeg"}
@@ -113,7 +142,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="4">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[3] > -50
                      ? this.state.data.dateArray[3]
                      : "leeg"}
@@ -121,7 +150,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="5">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[4] > -50
                      ? this.state.data.dateArray[4]
                      : "leeg"}
@@ -129,7 +158,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="6">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[5] > -50
                      ? this.state.data.dateArray[5]
                      : "leeg"}
@@ -137,7 +166,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="7">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[6] > -50
                      ? this.state.data.dateArray[6]
                      : "leeg"}
@@ -145,7 +174,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="8">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[7] > -50
                      ? this.state.data.dateArray[7]
                      : "leeg"}
@@ -153,7 +182,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="9">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[8] > -50
                      ? this.state.data.dateArray[8]
                      : "leeg"}
@@ -161,7 +190,7 @@ export class timeline extends Component {
                </div>
                <div className="dot" id="10">
                  <span></span>
-                 <a>
+                 <a onClick={this.handleClick}>
                    {this.state.data.dateArray[9] > -50
                      ? this.state.data.dateArray[9]
                      : "leeg"}
